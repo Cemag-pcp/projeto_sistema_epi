4c5fad7dddd12d778b24d8a081ebaa4c45efc607
TI <pcp@cemag.com.br>
2024-02-09 15:13:26 -0300

    Botoes de assinatura e recebimento itens execucao

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify,redirect, url_for,flash,session
 import datetime
+import pandas as pd
 import psycopg2  # pip install psycopg2
 import psycopg2.extras 
 from psycopg2.extras import execute_values
@@ -51,14 +52,25 @@ def pagina_inicial():
                             password=DB_PASS, host=DB_HOST)
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    s = """SELECT s.*, hs.status
+    s = """SELECT s.*, hs.status, a.assinatura
             FROM sistema_epi.tb_solicitacoes AS s
-            LEFT JOIN sistema_epi.tb_historico_solicitacoes AS hs ON s.id_solicitacao = hs.id_solicitacao;
+            LEFT JOIN sistema_epi.tb_historico_solicitacoes AS hs ON s.id_solicitacao = hs.id_solicitacao
+            LEFT JOIN sistema_epi.tb_assinatura AS a ON s.id = a.id;  -- Adicionei este LEFT JOIN
         """
     
-    cur.execute(s)
+    tb_solicitacoes = pd.read_sql_query(s, conn)
 
-    tb_solicitacoes = cur.fetchall()
+    print(tb_solicitacoes)
+
+    tb_solicitacoes = tb_solicitacoes.sort_values(by=['id','id_solicitacao'])
+
+    # Agrupar novamente e concatenar os valores da coluna codigo_item
+    tb_solicitacoes['codigo_item'] = tb_solicitacoes.groupby('id_solicitacao')['codigo_item'].transform(lambda x: ', '.join(x))
+
+    # Agrupar pelo id_solicitacao e manter apenas a primeira linha de cada grupo
+    tb_solicitacoes = tb_solicitacoes.groupby('id_solicitacao').first().reset_index()
+
+    tb_solicitacoes = tb_solicitacoes.values.tolist()
 
     print(tb_solicitacoes)
 
@@ -77,8 +89,9 @@ def receber_assinatura():
         # Obtém o dataURL da assinatura
         id_solicitacao = data.get('id_solicitacao')
         dataURL = data.get('dataURL')
+        id = data.get('id')
 
-        cur.execute("INSERT INTO sistema_epi.tb_assinatura (id_solicitacao, assinatura) VALUES (%s, %s)", (id_solicitacao, dataURL))
+        cur.execute("INSERT INTO sistema_epi.tb_assinatura (id_solicitacao, assinatura,id) VALUES (%s, %s,%s)", (id_solicitacao, dataURL,id))
         conn.commit()
 
         # Exemplo de resposta de volta para o cliente
@@ -86,6 +99,45 @@ def receber_assinatura():
 
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
+
+@app.route('/dados-execucao', methods=['POST'])
+def dados_execucao():
+    
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                            password=DB_PASS, host=DB_HOST)
+    
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    data = request.get_json()
+
+    id = data.get('id')
+
+    query = f"""SELECT s.*, hs.status
+            FROM sistema_epi.tb_solicitacoes AS s
+            LEFT JOIN sistema_epi.tb_historico_solicitacoes AS hs ON s.id_solicitacao = hs.id_solicitacao
+            WHERE s.id = {id};"""
+
+    cur.execute(query)
+    execucao = cur.fetchall()
+
+    print(execucao)
+
+    dados = {
+        'id':execucao[0][0],
+        'id_solicitacao':execucao[0][1],
+        'matricula':execucao[0][2],
+        'codigo':execucao[0][3],
+        'quantidade':execucao[0][4],
+        'motivo':execucao[0][5],
+        'setor':execucao[0][6],
+        'funcionario':execucao[0][7],
+        'data':execucao[0][8],
+        'status':execucao[0][9]
+    }
+    print(execucao[0][0])
+
+    # Exemplo de resposta de volta para o cliente
+    return jsonify(dados)
 
 if __name__ == '__main__':
     app.run(debug=True)
